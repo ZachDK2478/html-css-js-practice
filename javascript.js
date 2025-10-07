d879d95cfd6922ee6c1f32784873635d4bf55a1d
ZachDK <zachariahspirit@gmail.com>
2025-10-07 15:27:03 -0400

    More in-depth Javascript function test.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-function favoriteAnimal(animal) {
-    return animal + " is my favorite animal!"
+function InfoList(name, surname, age, gender, favoriteSong, favoriteGame) {
+    return "My name is " + name + " " + surname + " and I am " + age + ", my gender is " + gender + ", my favorite song is " + favoriteSong + ", and my favorite video game is " + favoriteGame + ".";
 }
 
-console.log(favoriteAnimal('Goat'))
+console.log(InfoList('Ajax', 'Keeperton', 18, 'male', 'Never Gonna Give You Up by Rick Astley', 'Sonic Frontiers or DELTARUNE'));
